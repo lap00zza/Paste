2a718d870d40f5ca31b2227621b37c1613678b68
Jewel Mahanta <jewelmahanta@gmail.com>
2017-09-07 13:49:16 +0530

    ref (start_server): made the file easier to understand

--- start_server.sample.sh
+++ start_server.sample.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
-# Remember to fill in these before starting Paste
-# and rename it to 'start_server.sh'
-PGUSER=USER \
-PGHOST=HOST \
-PGPASSWORD=PASSWORD \
-PGDATABASE=DATABASE \
-PGPORT=PORT \
-SALT=SALT_FOR_HASHID \
+# This is the port on which Paste will be listening.
+export PASTE_PORT=3000
+
+# IP Address to which Paste will be bound. If you want
+# Paste to listen to external requests use "0.0.0.0"
+export PASTE_HOST="127.0.0.1"
+
+# Config for Postgres.
+export PGUSER="PGUSER"
+export PGHOST="PGHOST"
+export PGPASSWORD="PGPASSWORD"
+export PGDATABASE="PGDATABASE"
+export PGPORT=PGPORT
+
+# Salt for Hashids.
+export SALT="some_salt"
+
+# Start Paste.
 node ./dist/server.js
